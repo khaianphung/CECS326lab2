2e55558fe638f5b1be008b29b47749d86d2731d9
tamqtran <tamtran1997@gmail.com>
2018-03-05 00:20:47 -0800

    Update and rename sender2.cpp to sender251.cpp
    clean up code and rename to sender251.cpp

--- sender251.cpp
+++ sender251.cpp
@@ -1,17 +1,5 @@
 /* 
-
-This is a simple illustration of the use of:
-	ftok, msgget, msgsnd, msgrcv
-
-Program A will use a message queue created by Program B. 
-Then, they will pass messages back and forth.
-
-Program A sends the first message and reads the reply. Program A
-also sends a "fake" message to the msgQ that will never be read
-by anyone.
-
-Both child processes use message type mtype = 113 and 114.
-
+sender 251
 */
 
 #include <sys/types.h>
@@ -26,10 +14,7 @@ using namespace std;
 
 int main() {
 
-	// pause Program A
-	//sleep(3); 	// BAD programming - unreliable and potential bug
-			// do NOT use
-
+	// search for the queue
 	int qid = msgget(ftok(".",'u'), 0);
 
 	// declare my message buffer
@@ -43,7 +28,7 @@ int main() {
 	
 	strcpy(msg.greeting, "Hello there");	
 	cout << getpid() << ": sends greeting" << endl;
-	msg.mtype = 251; 	// set message type mtype = 117
+	msg.mtype = 251; 	// set message type mtype = 251
 	msgsnd(qid, (struct msgbuf *)&msg, size, 0); // sending
 
 	msgrcv(qid, (struct msgbuf *)&msg, size, 251, 0); // reading
@@ -51,8 +36,4 @@ int main() {
 	cout << "reply: " << msg.greeting << endl;
 	cout << getpid() << ": now exits" << endl;
 
-	
-	
 }
-
-
